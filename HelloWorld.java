c47c5e2d2471ad571db9cb120450f8ded1ce6be3
soundousnidar <157213596+soundousnidar@users.noreply.github.com>
2025-12-23 19:56:14 +0100

    Add HelloWorld class

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("Hello from MultiBranch Pipeline!");
+        System.out.println("TP DevOps - Jenkins + GitHub");
+        System.out.println("Webhook automatique actif!");
+        System.out.println("=================================");
+    }
+}
